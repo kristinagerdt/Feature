d3502b1412d8aaa1f33e1691ad5a6b50a3203840
kristinagerdt <gerdt.kristina@gmail.com>
2021-07-05 19:51:39 +0200

    gh-8 add method getAllFeatures(), add Convereter class

--- src/main/java/test/kgerdt/feature/service/impl/FeatureServiceImpl.java
+++ src/main/java/test/kgerdt/feature/service/impl/FeatureServiceImpl.java
@@ -1,16 +1,20 @@
 package test.kgerdt.feature.service.impl;
 
 import org.springframework.stereotype.Service;
+import test.kgerdt.feature.model.entity.Feature;
 import test.kgerdt.feature.model.web.FeatureWebDto;
 import test.kgerdt.feature.service.FeatureService;
+import test.kgerdt.feature.tool.Converter;
 import test.kgerdt.feature.tool.Parser;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class FeatureServiceImpl implements FeatureService {
 
     private final Parser parser;
+    private List<Feature> features;
 
     public FeatureServiceImpl(Parser parser) {
         this.parser = parser;
@@ -18,7 +22,10 @@ public class FeatureServiceImpl implements FeatureService {
 
     @Override
     public List<FeatureWebDto> getAllFeatures() {
-        return null;
+        return features
+                .stream()
+                .map(Converter::convertToFeatureWebDto)
+                .collect(Collectors.toList());
     }
 
     @Override
