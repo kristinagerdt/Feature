f8d39dc6679f52bb79e8fc847d70bfdc7409a612
kristinagerdt <gerdt.kristina@gmail.com>
2021-07-05 18:34:08 +0200

    gh-1 add initial files

---
+++ src/test/java/test/kgerdt/feature/FeatureApplicationTests.java
@@ -0,0 +1,12 @@
+package test.kgerdt.feature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
