374ba44fcb8ee595c0d826f89975ecdc73b7dfcb
kristinagerdt <gerdt.kristina@gmail.com>
2021-07-05 21:05:53 +0200

    gh-14 add noImageByteArray property, app.properties, no-image.png, change source-data.json

--- src/main/java/test/kgerdt/feature/tool/Parser.java
+++ src/main/java/test/kgerdt/feature/tool/Parser.java
@@ -1,16 +1,22 @@
 package test.kgerdt.feature.tool;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import test.kgerdt.feature.model.entity.Acquisition;
 import test.kgerdt.feature.model.entity.Feature;
 import test.kgerdt.feature.model.entity.Property;
 
+import javax.annotation.PostConstruct;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
@@ -18,9 +24,29 @@ import java.util.List;
 @Component
 public class Parser {
 
-    public List<Feature> loadAllFeatures() {
-        String sourceJson = "src/main/resources/static/source-data.json";
+    @Value("${source.json}")
+    private String sourceJson;
 
+    @Value("${source.noImage}")
+    private String sourceNoImage;
+    private byte[] noImageByteArray;
+
+    @PostConstruct
+    public void init() {
+        noImageByteArray = getNoImageByteArray(sourceNoImage);
+    }
+
+    private byte[] getNoImageByteArray(String path) {
+        try {
+            File initialFile = new File(path);
+            InputStream inputStream = FileUtils.openInputStream(initialFile);
+            return IOUtils.toByteArray(inputStream);
+        } catch (IOException e) {
+            return new byte[0];
+        }
+    }
+
+    public List<Feature> loadAllFeatures() {
         JSONParser jsonParser = new JSONParser();
 
         try (FileReader reader = new FileReader(sourceJson)) {
@@ -68,8 +94,12 @@ public class Parser {
         property.setAcquisition(acquisition);
 
         String quicklookString = (String) propertyObject.get("quicklook");
-        byte[] quicklook = Base64.getDecoder().decode(quicklookString);
-        property.setQuicklook(quicklook);
+        if (quicklookString != null) {
+            byte[] quicklook = Base64.getDecoder().decode(quicklookString);
+            property.setQuicklook(quicklook);
+        } else {
+            property.setQuicklook(noImageByteArray);
+        }
 
         return property;
     }
